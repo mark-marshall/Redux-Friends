2b7a18b9f5b00bb9a953c272d44d59d944ce329f
mark-marshall <mark.marshallgp@gmail.com>
2019-03-06 14:52:42 +0000

    [MVP] Added Edit and Add Friend Components

--- friends/src/Components/AddFriend.jsx
+++ friends/src/Components/AddFriend.jsx
@@ -3,8 +3,30 @@ import React from 'react';
 export default function AddFriend() {
   return (
     <div>
-      <input />
-      <button />
+       <input
+          placeholder="Name"
+          name="name"
+          type="text"
+          // onChange={event => addFriendHandler(event)}
+          // value={addFriend.name}
+        />
+        <input
+          placeholder="Age"
+          name="age"
+          type="number"
+          // onChange={event => addFriendHandler(event)}
+          // value={addFriend.age}
+        />
+        <input
+          placeholder="Email"
+          name="email"
+          type="email"
+          // onChange={event => addFriendHandler(event)}
+          // value={addFriend.email}
+        />
+        <button className="addButton" /*onClick={postFriend}*/ type="submit">
+          Add Friend
+        </button>
     </div>
   );
 }
--- friends/src/Components/EditFriend.jsx
+++ friends/src/Components/EditFriend.jsx
@@ -3,8 +3,33 @@ import React from 'react';
 export default function EditFriend() {
   return (
     <div>
-      <input />
-      <button />
+      <input
+        placeholder="Name"
+        name="name"
+        type="text"
+        // onChange={event => addFriendHandler(event)}
+        // value={addFriend.name}
+      />
+      <input
+        placeholder="Age"
+        name="age"
+        type="number"
+        // onChange={event => addFriendHandler(event)}
+        // value={addFriend.age}
+      />
+      <input
+        placeholder="Email"
+        name="email"
+        type="text"
+        // onChange={event => addFriendHandler(event)}
+        // value={addFriend.email}
+      />
+      <button type="submit" /*</div>onClick={updateFriend}*/>
+        Edit Friend
+      </button>
+      <button type="submit" /*onClick={cancelEdit}*/ className="cancelButton">
+        X
+      </button>
     </div>
   );
 }
